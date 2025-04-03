d1fe222355b596ca96b80abb14eda1940568613b
Patrick Diamitani <patrick.diamitani@gmail.com>
2025-04-02 21:27:09 -0500

    Create init.js

---
+++ api/oauth/init.js
@@ -0,0 +1,17 @@
+import { google } from 'googleapis';
+
+export default async function handler(req, res) {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    `${process.env.BASE_URL}/api/oauth/callback`
+  );
+
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: ['https://www.googleapis.com/auth/drive.file'],
+  });
+
+  res.redirect(authUrl);
+}
+
