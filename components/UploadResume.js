1b3370b262f57c40db5caad29053bb7acdd7891d
Patrick Diamitani <patrick.diamitani@gmail.com>
2025-04-02 23:53:29 -0500

    Update UploadResume.js

--- components/UploadResume.js
+++ components/UploadResume.js
@@ -1,1 +1,13 @@
+export default function UploadResume({ onUpload }) {
+  const handleFile = (e) => {
+    const file = e.target.files[0];
+    onUpload(file);
+  };
+
+  return (
+    <div className="my-4">
+      <input type="file" onChange={handleFile} accept=".pdf,.doc,.docx" />
+    </div>
+  );
+}
 
