7e40c8a8d2ed3384f683088cf3dfe62bf2826b79
Patrick Diamitani <patrick.diamitani@gmail.com>
2025-04-02 23:53:53 -0500

    Update SaveToDrive.js

--- components/SaveToDrive.js
+++ components/SaveToDrive.js
@@ -1,1 +1,8 @@
+export default function SaveToDrive({ onClick }) {
+  return (
+    <button onClick={onClick} className="bg-green-600 text-white px-4 py-2 mt-2">
+      Save to Google Drive
+    </button>
+  );
+}
 
