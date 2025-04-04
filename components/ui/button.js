a435e0f0903bfd7bfb207faff24bf132af7ef57a
Patrick Diamitani <patrick.diamitani@gmail.com>
2025-04-03 20:48:30 -0500

    Update button.js

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,3 +1,9 @@
-export function Button({ children, ...props }) {
-  return <button className="px-4 py-2 bg-blue-600 text-white rounded" {...props}>{children}</button>;
-}
+import React from "react";
+
+export const Button = ({ label, onClick }) => {
+  return (
+    <button className="button" onClick={onClick}>
+      {label}
+    </button>
+  );
+};
