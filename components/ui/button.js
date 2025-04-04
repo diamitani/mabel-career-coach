8e64149b272cc2046f4647967f7d0933f4eec377
Patrick Diamitani <patrick.diamitani@gmail.com>
2025-04-03 21:55:56 -0500

    Update button.js

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React from 'react';
 
 export const Button = ({ label, onClick }) => {
   return (
-    <button className="button" onClick={onClick}>
+    <button
+      onClick={onClick}
+      className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700 transition-all"
+    >
       {label}
     </button>
   );
