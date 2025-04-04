57e3467d79c46a06933e88035130c2e6b0ad3ba2
Patrick Diamitani <patrick.diamitani@gmail.com>
2025-04-03 21:56:20 -0500

    Update card.js

--- components/ui/card.js
+++ components/ui/card.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
 export const Card = ({ children }) => {
   return (
-    <div className="card">
+    <div className="border rounded-lg p-4 shadow-lg">
       {children}
     </div>
   );
 };
 
 export const CardContent = ({ children }) => {
-  return <div className="card-content">{children}</div>;
+  return <div className="p-4">{children}</div>;
 };
