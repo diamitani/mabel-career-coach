d1b00ebabb503a3af8f5cbfe00f7fe03cf9fb15b
Patrick Diamitani <patrick.diamitani@gmail.com>
2025-04-03 22:03:58 -0500

    Update oauth.js

--- api/google-temp/oauth.js
+++ api/google-temp/oauth.js
@@ -1,22 +1,16 @@
-import { google } from 'googleapis';
+import { google } from "googleapis";
 
 export default async function handler(req, res) {
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/google/oauth/callback`
+    `${process.env.BASE_URL}/api/google/callback`
   );
 
-  const scopes = [
-    'https://www.googleapis.com/auth/drive.file'
-  ];
-
-  const url = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: scopes,
-    prompt: 'consent',
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: ["https://www.googleapis.com/auth/drive.file"],
   });
 
-  res.redirect(url);
+  res.redirect(authUrl);
 }
-
